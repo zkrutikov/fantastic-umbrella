c1ca29598b55112970e1cb2a7d66ab76306e77b8
Zak Krutikov <zak.krutikov@gmail.com>
2021-12-19 23:01:10 -0500

    finished models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -7,7 +7,9 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
